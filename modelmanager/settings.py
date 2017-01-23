a831bfae1d2b4dc7df074af35b055418dd6f5508
Michel Wortmann <wortmann@pik-potsdam.de>
2017-01-23 22:29:26 +0100

    Introduce djangoapp and add a bunch of ideas

---
+++ modelmanager/settings.py
@@ -0,0 +1,11 @@
+#/usr/bin/env python
+'''
+The modelmanager settings module contains everything concerning the setup, management and validation of the project settings defined in the settings .mm/settings.json file.
+'''
+
+
+DEFAULTS = {
+        ('store_input_functions', 'Functions that return input-specific values such as parameter values etc. Function output should be either single values with basic types, iterables of basic types or a pandas dataframe'):
+            [],
+        ()
+        }
