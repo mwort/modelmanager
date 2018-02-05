66312752eb0d43081e1aa4029962edd7092fbd8f
Michel Wortmann <git@michelwortmann.de>
2018-02-05 12:25:43 +0100

    Clean up model fields.

--- modelmanager/plugins/browser/models.py
+++ modelmanager/plugins/browser/models.py
@@ -2,13 +2,8 @@ from django.db import models
 
 
 class Run(models.Model):
-    class Meta:
-#        abstract = True
-        ordering = ['-pk']
     time = models.DateTimeField('Time', auto_now_add=True)
-    TYPES = ['testing', 'calibration', 'validation', 'prediction']
-    type = models.CharField('Type', max_length=32, default='testing',
-                            choices=zip(TYPES, TYPES))
+    tags = models.CharField(max_length=1024, blank=True)
     notes = models.TextField('Notes', blank=True)
 
 
@@ -20,10 +15,10 @@ class TaggedValue(models.Model):
     DECIMAL_PLACES = 8
 
     run = models.ForeignKey(Run, on_delete=models.CASCADE)
-    tags = models.CharField(max_length=1024)
+    name = models.CharField(max_length=128)
     value = models.DecimalField(max_digits=MAX_DIGITS,
                                 decimal_places=DECIMAL_PLACES)
-    notes = models.TextField('Notes', blank=True)
+    tags = models.CharField(max_length=1024, blank=True)
 
 
 class Parameter(TaggedValue):
