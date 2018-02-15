e1f071385ce1399012bdfd0bcf6ac8e0bc825d01
Michel Wortmann <git@michelwortmann.de>
2018-02-15 08:50:48 +0100

    Fixed dependencies and added commented out requirements file for plugins.

--- setup.py
+++ setup.py
@@ -12,11 +12,13 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 # get the dependencies and installs
-with open(path.join(here, 'requirements_dev.txt'), encoding='utf-8') as f:
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
     all_reqs = f.read().split('\n')
 
-install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
-dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
+install_requires = [x.strip() for x in all_reqs
+                    if 'git+' not in x and not x.startswith('#')]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs
+                    if x.startswith('git+')]
 
 setup(
     name='modelmanager',
