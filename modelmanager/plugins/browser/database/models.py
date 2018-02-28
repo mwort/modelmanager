841a6fc1fdc9fbb6fa9b07e1eac888493c3ec3bb
Michel Wortmann <git@michelwortmann.de>
2018-02-28 08:49:56 +0100

    Copy inserted browser files before saving to browser/files/runs . This avoids the SuspeciousFileOperation error but leads to duplicated files as Django thinks it already exists.

--- modelmanager/plugins/browser/database/models.py
+++ modelmanager/plugins/browser/database/models.py
@@ -2,9 +2,12 @@
 This is a collection of meta models to be subclassed in the projects
 browser/models.py file.
 """
+import os
 import os.path as osp
+import shutil
 
 from django.db import models
+from django.conf import settings
 from django.core.files import File as djFile
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
@@ -37,40 +40,53 @@ class TaggedValue(RunTagged):
                                 decimal_places=DECIMAL_PLACES)
 
 
-def run_file_path(instance, filename):
-    return osp.join('runs', str(instance.run.pk), filename)
+def upload_to_path(instance, filename):
+    return osp.join(instance.SUBDIR, str(instance.run.pk), filename)
 
 
 class File(RunTagged):
     class Meta:
         abstract = True
 
-    file = models.FileField(upload_to=run_file_path)
+    SUBDIR = 'runs'
+    file = models.FileField(upload_to=upload_to_path)
 
     def __init__(self, *args, **kwargs):
         # handle different file objects on construction
         if 'file' in kwargs:
-            f = kwargs.pop('file')
-            if type(f) == str:
-                f = file(f, 'rb+')
-            # try convert anything that isnt a file by now into a django File
-            # or a django InMemoryUploadedFile
-            try:
-                if not isinstance(f, file):
-                    errmsg = ('If file isnt a file instance, a filename needs '
-                              ' to be passed. %r' % f)
-                    assert 'filename' in kwargs, errmsg
-                    f = InMemoryUploadedFile(f, None, kwargs.pop('filename'),
-                                             None, f.len, None)
-                else:
-                    f = djFile(f)
-                f.readable()
-            except (TypeError, AttributeError):
-                raise TypeError('Cant convert %s to a ' % f +
-                                'django.core.files.File instance.')
-            kwargs['file'] = f
-
+            kwargs['file'] = self.handle_file(**kwargs)
+            if 'filename' in kwargs:
+                kwargs.pop('filename')
         super(File, self).__init__(*args, **kwargs)
+
+    def handle_file(self, **kwargs):
+        """
+        Converts a filepath, a file object or a buffer object to a safe and
+        valid django.core.files.File using the kwargs of the __init__.
+        """
+        f = kwargs['file']
+        media_root = settings.MEDIA_ROOT
+        if isinstance(f, file) or type(f) is str:
+            oldp = osp.abspath(f if type(f) == str else f.name)
+            if oldp.startswith(media_root):
+                newp = oldp
+            else:
+                di = osp.join(media_root, self.SUBDIR, str(kwargs['run'].pk))
+                newp = osp.join(di, osp.basename(oldp))
+                if not osp.exists(di):
+                    os.makedirs(di)
+                errmsg = "File already exists for this run: " + newp
+                assert not osp.exists(newp), errmsg
+                shutil.copy(oldp, newp)
+            # create django file
+            f = djFile(file(newp, 'rb+'))
+        else:
+            errmsg = ('If file isnt a file instance, a filename needs '
+                      ' to be passed. %r' % f)
+            assert 'filename' in kwargs, errmsg
+            f = InMemoryUploadedFile(f, None, kwargs.pop('filename'),
+                                     None, f.len, None)
+        return f
 
     def delete(self):
         self.file.delete()
